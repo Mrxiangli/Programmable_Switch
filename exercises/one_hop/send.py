94b6fcc1135f63a9788b783232a9f1745cfe3591
Jonny Li <li2068@purdue.edu>
2022-04-17 16:47:23 -0400

    l

--- exercises/one_hop/send.py
+++ exercises/one_hop/send.py
@@ -6,6 +6,7 @@ import pdb
 from scapy.all import IntField, BitField, IP, TCP, Ether, get_if_hwaddr, get_if_list,Packet, srp1, bind_layers, srp1flood,sendp
 import csv
 import time
+import sys
 class Klass(Packet):
     name = "Klass"
     fields_desc=[
@@ -55,6 +56,7 @@ def main():
     with open("testing-data.csv","r") as test:
         csv_reader = csv.reader(test)
         for row in csv_reader:
+            time.sleep(0.01)
             pkt =  Ether(src=get_if_hwaddr(iface), dst='00:00:00:00:00:00')
             pkt = pkt / IP(dst=addr) / TCP(dport=1234, sport=random.randint(49152,65535)) / Klass(hash=0, X10=int(row[10]),X11=int(row[11]),X14=int(row[14]),X17=int(row[17]),X27=int(row[27]), start=time.time_ns(), truth=int(int(row[-1])/1000))
             sendp(pkt, iface=iface, verbose=False)
